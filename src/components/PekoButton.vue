2f0f70d792b5327591befa5f4fdf27d98cfd8a36
BillSavart <energyy0512@gmail.com>
2021-10-08 23:27:27 +0800

    Initial Commit

---
+++ src/components/PekoButton.vue
@@ -0,0 +1,44 @@
+<template>
+    <h3 class="counter">
+        {{ counter }}
+    </h3>
+    <img :src="pekoImg" alt="Pekora image" width="400" heigth="500" @mousedown="peko" @mouseup="noPeko">
+</template>
+
+<script>
+const pekoAudio = require('../assets/peko.mp3');
+
+export default {
+    data() {
+        return { 
+            pekoAudio,
+            pekoImg: require('../assets/pekora.jpg'),
+            counter: 0
+        };
+    },
+    methods: {
+        getAudio() {
+            return this.pekoAudio;
+        },
+        peko() {
+            this.pekoImg = require('../assets/pekoClicked.jpg');
+            this.counter++;
+
+            let audio = new Audio(this.pekoAudio);
+            audio.addEventListener("canplaythrough", () => {
+                audio.play();
+            });
+        },
+        noPeko() {
+            this.pekoImg = require('../assets/pekora.jpg');
+        }
+    }
+}
+</script>
+
+<style>
+    .counter {
+        margin: 10px auto;
+        font-size: 2em;
+    }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img src="../assets/poppeko.png" alt="poppeko">
+    <PekoButton/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import PekoButton from '../components/PekoButton.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
+  components: { PekoButton }
 }
 </script>
+
+<style>
+  body {
+    background-image: url('../assets/pekora_bg.jpg');
+  }
+</style>
